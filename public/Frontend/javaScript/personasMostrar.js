9144b9dd3c26b6eaf574c203c7aef4ba6cfb0305
MambaxX55 <olgaarenasrosario22@gmail.com>
2024-05-20 16:58:23 +0200

    editado para que se vean las personas (todavia no se ven y redirige a productos)

--- public/Frontend/javaScript/personasMostrar.js
+++ public/Frontend/javaScript/personasMostrar.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             let deathDate = person.deathDate ? person.deathDate : "Actualidad";
 
             personaElemento.innerHTML = `
-                <strong>${person.id}: ${person.name}</strong>
+                <a href="personasMostrar.html" class="linkIndex" data-persona="${person.id}"><strong>${person.id}: ${person.name}</strong></a>
                 <br>
                 <img src="${imageUrl}" width="120" height="120" class="imagenIndex">
                 <p>${person.birthDate} - ${deathDate}</p>
@@ -29,10 +29,38 @@ document.addEventListener("DOMContentLoaded", async () => {
 
             contenedorPersonas.appendChild(personaElemento);
         });
+
+        // Agregar event listener a los enlaces después de que se han agregado al DOM
+        document.querySelectorAll('.linkIndex').forEach(link => {
+            link.addEventListener('click', async function(event) {
+                event.preventDefault(); // Evitar que se siga el enlace
+                const personId = this.getAttribute('data-persona');
+                await llamarFuncionPersona(personId);
+                // Redirigir a la página después de que se complete la función
+                window.location.href = "personasMostrar.html";
+            });
+        });
     } catch (error) {
-        console.error("Error fetching persons:", error);
+        console.error("Error fetching personas:", error);
     }
 });
 
-
-
+async function llamarFuncionPersona(index) {
+    const token = sessionStorage.getItem("access_token");
+    try {
+        const response = await fetch(`http://127.0.0.1:8000/api/v1/persons/${index}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        if (response.ok) {
+            const person = await response.json();
+            sessionStorage.setItem('personasVer', JSON.stringify(person.person));
+        } else {
+            console.error('Error fetching person:', response.statusText);
+        }
+    } catch (error) {
+        console.error("Error fetching person:", error);
+    }
+}
--- public/Frontend/javaScript/personasVer.js
+++ public/Frontend/javaScript/personasVer.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const persona = obtenerPersona();
-
-    document.getElementById("personaImagen").src = persona.imagen;
-    document.getElementById("personaNombre").textContent = persona.nombre;
-    document.getElementById("personaNacimiento").textContent = `Nacimiento: ${persona.nacimiento}`;
-    document.getElementById("personaDefuncion").textContent = `Defunción: ${persona.defuncion}`;
-    document.getElementById("wikipedia").src = persona.url;
-
+    document.getElementById("personaImagen").src = persona.imageUrl;
+    document.getElementById("personaNombre").textContent = persona.name;
+    document.getElementById("personaNacimiento").textContent = `Nacimiento: ${persona.birthDate}`;
+    document.getElementById("personaDefuncion").textContent = `Defunción: ${persona.deathDate}`;
+    if(persona.wikiUrl === null || persona.wikiUrl === ""){
+        document.getElementById("wikipedia").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUzzTNcjuqNynQaX1mu0n_uyJp6-LjgbTqoWCrZzrqFw&s";
+    }else{
+        document.getElementById("wikipedia").src = persona.wikiUrl;
+    }
 
 })
 
