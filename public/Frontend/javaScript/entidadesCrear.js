e68a49ddeab94db24057cf30fc203213ca406a70
MambaxX55 <olgaarenasrosario22@gmail.com>
2024-05-20 14:45:58 +0200

    modificado crear entidades para usar la api

--- public/Frontend/javaScript/entidadesCrear.js
+++ public/Frontend/javaScript/entidadesCrear.js
@@ -1,62 +1,131 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Cuando se hace click en crearProducto...
-    document.getElementById("crearEntidad").addEventListener("click", crearEntidad);
+    // Cuando se hace click en crearEntidad...
+    document.getElementById("crearEntidad").addEventListener("click", async event => {
+        event.preventDefault();
 
+        const token = sessionStorage.getItem("access_token");
+        const form = document.getElementById("entidad");
+        const formData = new FormData(form);
+
+        try {
+            const response = await fetch("http://127.0.0.1:8000/api/v1/entities", {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error('No se ha podido crear la entidad');
+            }
+
+            const data = await response.json();
+            takePersonas(data.entity.id);
+            takeProductos(data.entity.id);
+            alert("Entidad creada");
+
+        } catch (error) {
+            alert(error.message);
+        }
+    });
     // Cuando se hace click en volverWriter ...
     document.getElementById("volverWriter").addEventListener("click", function () {
         window.location.href = "indexWriter.html";
     });
 
     buildPersonas();
-})
+    buildProducts();
+});
 
-function crearEntidad() {
-    const entidad = document.getElementById("newEntity").value;
-    const inicio = document.getElementById("newStart").value;
-    const final = document.getElementById("newEnd").value;
-    const url = document.getElementById("newUrl").value;
-    const image = document.getElementById("newImage").value;
-    const personas = takePersonas();
-
-    if (entidad === "" || inicio === "") {
-        alert("Hay campos sin rellenar \nPor favor rellene los campos con (*)");
-    } else {
-        agregarEntidad(entidad, inicio, final, url, image, personas);
-        alert("Entidad creada");
-        event.preventDefault();
-        window.location.href = "indexWriter.html";
-    }
-}
-
-function buildPersonas() {
+async function buildPersonas() {
     const elemento = document.getElementById('PersonasEntidad');
+    await fetch("http://127.0.0.1:8000/api/v1/persons", {
 
-    const personas = JSON.parse(localStorage.getItem("personas")) || [];
-
-    for (const persona of personas) {
-
-        const option = document.createElement("option");
-        option.value = persona.nombre;
-        option.textContent = persona.nombre;
-        elemento.appendChild(option);
-    }
+    })
+        .then(response =>{
+            if(!response.ok){
+                console.log("No hay personas");
+            }else{
+                return response.json();
+            }
+        })
+        .then(data =>{
+            if(data != undefined){
+                const personas = data.persons;
+                for(const person of personas){
+                    const option = document.createElement("option");
+                    option.value = person.person.id;
+                    option.textContent = person.person.name;
+                    elemento.appendChild(option);
+                }
+            }
+        });
 }
 
-function takePersonas(){
+async function buildProducts() {
+    const elemento = document.getElementById('ProductosEntidad');
+    await fetch("http://127.0.0.1:8000/api/v1/products", {
+
+    })
+        .then(response =>{
+            if(!response.ok){
+                console.log("No hay productos");
+            }else{
+                return response.json();
+            }
+        })
+        .then(data =>{
+            if(data != undefined){
+                const products = data.products;
+                for(const product of products){
+                    const option = document.createElement("option");
+                    option.value = product.product.id;
+                    option.textContent = product.product.name;
+                    elemento.appendChild(option);
+                }
+            }
+        });
+}
+
+function takePersonas(idEntidad){
     const select = document.getElementById("PersonasEntidad");
     const respuestas = select.selectedOptions;
-    const personas = [];
-    for (let i = 0; i < respuestas.length; i++){
-        personas.push(respuestas[i].label);
+
+    for(let i = 0; i < respuestas.length; i++){
+        let idPersona = respuestas[i].value;
+        aniadirRelacionPersona(idPersona, idEntidad);
     }
-    return personas;
 }
 
+function takeProductos(idEntidad){
+    const select = document.getElementById("ProductosEntidad");
+    const respuestas = select.selectedOptions;
 
+    for(let i = 0; i < respuestas.length; i++){
+        let idProducto = respuestas[i].value;
+        aniadirRelacionProducto(idProducto, idEntidad);
+    }
+}
 
-function agregarEntidad(nombre, inicio, final, url, imagen, personas) {
-    const newE = new Entidad(nombre, inicio, final, url, imagen, personas);
-    const entidades = JSON.parse(localStorage.getItem("entidades")) || [];
-    entidades.push(newE);
-    localStorage.setItem('entidades', JSON.stringify(entidades));
+async function aniadirRelacionPersona(idPersona, idEntidad){
+    const token = sessionStorage.getItem("access_token");
+
+    await fetch(`http://127.0.0.1:8000/api/v1/entities/${idEntidad}/persons/add/${idPersona}`,{
+        method: 'PUT',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+}
+
+async function aniadirRelacionProducto(idProducto, idEntidad){
+    const token = sessionStorage.getItem("access_token");
+
+    await fetch(`http://127.0.0.1:8000/api/v1/entities/${idEntidad}/products/add/${idProducto}`,{
+        method: 'PUT',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
 }
